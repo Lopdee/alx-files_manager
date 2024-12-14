8ea73e2736279d7e75427f7b9763947d9dbd1dc0
samueleffiongjacob <samueleffiongjacob@.com>
2024-12-14 02:26:46 +0100

    fixed

--- libs/boot.js
+++ libs/boot.js
@@ -5,7 +5,7 @@ const startServer = (api) => {
   try {
     envLoader();
     const port = process.env.PORT || 5000;
-    const env = process.env.NODE_ENV || 'dev';
+    const env = process.env.npm_lifecycle_event || 'dev';
     api.listen(port, () => {
       console.log(`[${env}] API has started listening at port: ${port}`);
     });
